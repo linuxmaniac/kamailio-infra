3f936bed56d6b81ad0ad7c208471a2bcc93f4fdf
Victor Seva <linuxmaniac@torreviejawireless.org>
2025-06-19 23:43:05 +0200

    Initial commit

---
+++ opentofu/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  # backend "s3" {
+  #   bucket         = var.bucket_name
+  #   key            = "path/to/state.tfstate"
+  #   region         = var.aws_region
+  #   dynamodb_table = local.dynamodb_table_name
+  # }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
---
+++ opentofu/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  type        = string
+  default     = "kamailio-infra"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "eu-central-1"
+}
