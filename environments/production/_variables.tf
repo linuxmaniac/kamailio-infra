f8c4abb94868938530c88e791a1e94245ffd0d9b
Victor Seva <linuxmaniac@torreviejawireless.org>
2026-09-05 22:43:31 +0200

    environments [prod]: refresh master AMI

--- environments/production/_variables.tf
+++ environments/production/_variables.tf
@@ -46,7 +46,7 @@ variable "jenkins_disk_volume" {
 ## VM details
 variable "ami" {
   type        = string
-  default     = "ami-033769b873d553d2d"
+  default     = "ami-07b03f258d17ce5c0"
   description = "AMI to use"
 }
 variable "instance_type" {
